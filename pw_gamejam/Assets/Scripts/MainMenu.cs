f20038d65ee133fa8cfba6343d9e8a2ac563fc40
Basti <s.f.falser@gmail.com>
2021-10-01 13:37:31 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- pw_gamejam/Assets/Scripts/MainMenu.cs
+++ pw_gamejam/Assets/Scripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("GameScene");
     }
 
     public void ExitGame()
